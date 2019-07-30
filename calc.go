35d60013df4faadb9a26ba5dc7add1bc35f6d37e
Abhijeet Sarkar <53407084+sarkarabhijeet0512@users.noreply.github.com>
2019-07-30 20:20:52 +0530

    Add files via upload

--- calc.go
+++ calc.go
@@ -1,31 +1,39 @@
 package main
 
-import "fmt"
-func main(){
-var num1,num2 float64
-fmt.Println("enter the first number")
-fmt.Scan(&num1)
-fmt.Println("enter the second number")
-fmt.Scan(&num2)
-var ch string 
-fmt.Println("Choose operation to perform (+,-,*,/)")
-fmt.Scan(&ch)
-switch ch{ 	
-	case "+":
-		fmt.Println(num1+num2)
-		break
-	case "-":
-		fmt.Println(num1-num2)
-		break
-	case "*":
-		fmt.Println(num1*num2)
-		break
-	case "/":
-		fmt.Println(num1/num2)
-		break	
-	default:
-		fmt.Println("error")
-	
-}
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	var num1, num2 int
+	var ch string
+	fmt.Println("Choose operation to perform (+,-,*,/)")
+	fmt.Scan(&ch)
+	if ch == "+" || ch == "-" || ch == "*" || ch == "/" {
+		_, err := fmt.Scan(&num1, &num2)
+		if err != nil {
+			fmt.Println("Invalid number format;", err)
+			os.Exit(0)
+		}
+		switch ch {
+		case "+":
+			fmt.Println(num1 + num2)
+			break
+		case "-":
+			fmt.Println(num1 - num2)
+			break
+		case "*":
+			fmt.Println(num1 * num2)
+			break
+		case "/":
+			fmt.Println(num1 / num2)
+			break
+		default:
+			fmt.Println("invalid input\n use appropriate values")
+
+		}
+	} else {
+		fmt.Println("use valid operations")
+	}
 }
